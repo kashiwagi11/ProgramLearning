117b85da506ed5ce598c815d5bec824f2d25b76c
kashiwagi11 <kashiwagi@beingcorp.co.jp>
2022-10-27 14:42:33 +0900

    phpアプリケーションを作成 指定したamazonのurlから商品名を取得するアプリケーション

---
+++ PHP/application1-amazon-scraping/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+</head>
+
+<body>
+    <p>amazonの商品名を取得します</p>
+    <form action="amazon-scraping.php" method="post">
+        <label>
+            urlを入力
+            <input type="text" name="url" required>
+        </label>
+        <br><br>
+        <input type="submit" value="送信">
+    </form>
+
+</body>
+
+</html>
